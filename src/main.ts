d19b80de69ad222abc8cf29f3596a4b14ddd227c
archibald <archibaldofgenua@yandex.ru>
2024-01-07 23:04:53 +0300

    function declared

--- src/main.ts
+++ src/main.ts
@@ -40,3 +40,31 @@ class CommentThread implements IComment {
     this.comments = this.comments.filter((comment) => commentId !== comment.id);
   }
 }
+
+export function getRandomHumanImage() {
+  const imgContainer = document.querySelector('.image-container') as HTMLDivElement;
+  // Генерируем случайный номер для изображения
+  const randomNumber = Math.floor(Math.random() * 1000);
+
+  // Формируем URL для запроса рандомного изображения человека с Picsum
+  const imageUrl = 'https://picsum.photos/200/300' + randomNumber + '&category=people';
+  
+
+  // Получаем элемент изображения
+  const imageElement = document.getElementById('randomImage') as HTMLImageElement;
+
+  imageElement.onload = null;
+
+  // Устанавливаем новый путь к изображению
+  imageElement.src = imageUrl;
+  imgContainer.appendChild(imageElement);
+  imageElement.onload = () => {
+    // Изображение успешно загружено, выполните необходимые действия здесь
+    console.log('Изображение успешно загружено!');
+  };
+
+  // Добавляем обработчик события "error", чтобы обработать случаи ошибок загрузки изображения
+  imageElement.onerror = () => {
+    console.error('Ошибка при загрузке изображения');
+  };
+}
